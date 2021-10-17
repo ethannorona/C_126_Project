8a798339dd7b47950fa89f07ae779c886f78fdd9
ethannorona <77581569+ethannorona@users.noreply.github.com>
2021-10-17 17:40:56 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AlphabetRecognition from './screens/Camera.js'
+
+export default class App extends React.Component(){
+  render(){
+    return <AlphabetRecognition />;
+  }
+}
